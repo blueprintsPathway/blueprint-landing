fdcfbedcf7d98a07c561ee624fe44d192838e269
iamlimo <isaac.orija@gmail.com>
2022-10-13 21:57:37 +0100

    added router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      
+    <Routes>
+        <Route path="/" element={<App/>}>
+        <Route index element={<App/>} />
+          {/* <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+
+function LandingPage(){
+    
+    <>
+   <p> THis is test</p>
+    </>
+}
+
+export default LandingPage;
